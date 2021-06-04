15f7fd6b44ebde9563ed12f8ca3152c08f8e5a3b
sarbogast <sebastien.arbogast@gmail.com>
2021-06-04 19:07:55 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/image_layout_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_layout_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/400x600')),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/400x600')),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/600x400')),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/600x400')),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/400x600')),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/600x400')),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/600x400')),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Flexible(
+                        child: Image.network(
+                            'https://via.placeholder.com/400x600')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
